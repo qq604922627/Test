dba23e4ece90cf145d13ee087bfefdf002ee825f
unknown <604922627@qq.com>
2019-01-22 00:25:58 +0800

    d4

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
-    # above maps any URLs starting
-    # with rango/ to be handled by
-    # the rango application
     url(r'^admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="G:\pyworkspace\venv"
+$env:VIRTUAL_ENV="G:\tango_with_django_project\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
